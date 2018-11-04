c8f6599086768b07a239f6c34e4477f76aa50c9c
naoya.abe <micropig3402@gmail.com>
2018-11-04 07:11:27 +0000

    favorite

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,11 +10,18 @@ class User < ApplicationRecord
   
   
   has_many :microposts
+  
   has_many :relationships
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
 
+  has_many :favorites
+  has_many :likes, through: :favorites, source: :micropost
+  has_many :reverses_of_favorites, class_name: 'Favorite', foreign_key: 'micropost_id'
+  has_many :likeusers, through: :reverses_of_favorites, source: :user
+  
+  
   def follow(other_user)
     unless self == other_user
       self.relationships.find_or_create_by(follow_id: other_user.id)
@@ -28,6 +35,19 @@ class User < ApplicationRecord
 
   def following?(other_user)
     self.followings.include?(other_user)
+  end
+  
+  def like(micropost)
+    self.favorites.find_or_create_by(micropost_id: micropost.id)
+  end
+  
+  def unlike(micropost)
+    favorite=self.favorites.find_by(micropost_id: micropost.id)
+    favorite.destroy if favorite
+  end
+  
+  def liking?(micropost)
+    self.likes.include?(micropost)
   end
   
   def feed_microposts
